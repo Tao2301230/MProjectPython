8a1a0daffbb07376e4280d318595ead121f64b58
Tao <32484399+Tao2301230@users.noreply.github.com>
2018-08-07 02:43:59 +0100

    Add files via upload
    Update Hibench, Opentuner
    Have Problem in showing parameter summary.

--- MFrameUI.py
+++ MFrameUI.py
@@ -6,8 +6,9 @@ import ttk
 # Global Variable
 
 spark_parameter={}
-hibench_parameter={}
+hibench_parameter=[]
 opentuner_parameter={}
+task_id=0
 
 
 class BaseFrame(tk.Frame):
@@ -40,14 +41,15 @@ class BaseFrame(tk.Frame):
 '''
 completed: 
     Checkbox: bind with Save Button
-
-haven't implement          
     Add to Template, Add to New File bind with 2 Entries and 1 ComboBox
     Table to show final parameter
-    
+
+haven't implement          
+
 more:
     validation:
-        title, type bind with range
+        title: validate based on the version of Spark
+        type bind with range
 '''
 class SparkPSFrame(BaseFrame):
 
@@ -104,7 +106,7 @@ class SparkPSFrame(BaseFrame):
             Label(self, text=k).grid(row=crow, column=col)
             Label(self, text=v[0]).grid(row=crow, column=col+1)
             Label(self, text=v[1]).grid(row=crow, column=col+2)
-            cb=Checkbutton(self, variable=var)
+            cb = Checkbutton(self, variable=var)
             cb.grid(row=crow, column=col+3)
             self.cb_vars.append(var)
             self.spark_ps.append(k + ' ' + v[0] + ' ' + v[1])
@@ -116,20 +118,21 @@ class SparkPSFrame(BaseFrame):
 
 
         # Start From Scratch
-        self.ps_SFS = tk.Label(self, text='Add Manually: ')
+        self.ps_SFS = Label(self, text='Add Manually: ')
         self.ps_SFS.grid(row=11, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        self.ps_SFS_title = tk.Label(self, text='Parameter Title: ')
+        self.ps_SFS_title = Label(self, text='Parameter Title: ')
         self.ps_SFS_title.grid(row=12, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        self.parameter_title = tk.Entry(self)
+        self.parameter_title_var = StringVar()
+        self.parameter_title = Entry(self, textvariable=self.parameter_title_var)
         self.parameter_title.grid(row=12, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        self.ps_SFS_type = tk.Label(self, text='Parameter Type: ')
+        self.ps_SFS_type = Label(self, text='Parameter Type: ')
         self.ps_SFS_type.grid(row=13, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        pt = StringVar()
-        self.parameter_type = ttk.Combobox(self, textvariable=pt)
+        self.pt = StringVar()
+        self.parameter_type = ttk.Combobox(self, textvariable=self.pt)
         self.parameter_type['values']=('IntegerParameter', 'FloatParameter',
                                        'PowerOfTwoParameter', 'EnumParameter',
                                        'BooleanParameter', 'PermutationParameter')
@@ -138,18 +141,23 @@ class SparkPSFrame(BaseFrame):
         # self.bind('')
         self.parameter_type.grid(row=13, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        self.ps_range = tk.Label(self, text='Range / Enum: ')
+
+        self.ps_range = Label(self, text='Range / Enum: ')
         self.ps_range.grid(row=14, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        self.parameter_range_max = tk.Entry(self, width=5)
-        self.parameter_range_max.grid(row=14, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+        self.pr = StringVar()
+        self.parameter_range = Entry(self, textvariable=self.pr)
+        self.parameter_range.grid(row=14, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
 
 
-        self.ps_SFS_add = tk.Button(self, text='Add to Template')
-        self.ps_SFS_add.grid(row=15, column=2, padx=5, pady=5,sticky=tk.W + tk.E)
+        self.ps_SFS_add = Button(self, text='Add to Template', command=self.add_to_template)
+        self.ps_SFS_add.grid(row=11, column=2, padx=5, pady=5,sticky=tk.W + tk.E)
 
-        self.ps_SFS_add = tk.Button(self, text='Add to New File')
-        self.ps_SFS_add.grid(row=16, column=2, padx=5, pady=5,sticky=tk.W + tk.E)
+        self.ps_SFS_add = Button(self, text='Create Blank Draft', command=self.create_new_file)
+        self.ps_SFS_add.grid(row=11, column=3, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        self.ps_SFS_add = Button(self, text='Add to New File', command=self.add_to_new_file)
+        self.ps_SFS_add.grid(row=11, column=4, padx=5, pady=5,sticky=tk.W + tk.E)
 
     def save_template(self):
         result = [i.get() for i in self.cb_vars]
@@ -160,6 +168,49 @@ class SparkPSFrame(BaseFrame):
             spark_parameter[s[0]] = [s[1],s[2]]
         print spark_parameter
 
+        self.ps_add = Label(self, text='Successfully Added: ')
+        self.ps_add.grid(row=17, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        added_item = Label(self, text=', '.join(list(spark_parameter.keys())))
+        added_item.grid(row=17, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+
+
+    def add_to_template(self):
+        global spark_parameter
+        self.ps_add = Label(self, text='Successfully Added')
+        self.ps_add.grid(row=17, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        result = [i.get() for i in self.cb_vars]
+        final_spark_ps = [self.spark_ps[i] for i in range(len(result)) if result[i] == 1]
+
+        for item in final_spark_ps:
+            s = item.split()
+            spark_parameter[s[0]] = [s[1], s[2]]
+
+        spark_parameter[self.parameter_title_var.get()]=[self.pt.get(), self.pr.get()]
+
+        added_item = Label(self, text=', '.join(list(spark_parameter.keys())))
+        added_item.grid(row=17, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+
+
+    def create_new_file(self):
+        global spark_parameter
+        spark_parameter.clear()
+        self.ps_add = Label(self, text='Successfully Created: ')
+        self.ps_add.grid(row=17, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        added_item = Label(self, text=', '.join(list(spark_parameter.keys())))
+        added_item.grid(row=17, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+
+    def add_to_new_file(self):
+        global spark_parameter
+        spark_parameter[self.parameter_title_var.get()] = [self.pt.get(), self.pr.get()]
+
+        self.ps_add = Label(self, text='Successfully Added: ')
+        self.ps_add.grid(row=17, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        added_item = Label(self, text=', '.join(list(spark_parameter.keys())))
+        added_item.grid(row=17, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
 
 
 '''
@@ -200,31 +251,41 @@ class HibenchPSFrame(BaseFrame):
         hibench.wordcount.bigdata.datasize              1600000000000
         '''
 
-        self.workload = StringVar()
+        self.workload=StringVar()
+        self.hibench_task = StringVar()
+
         col = 2
 
         for r in ('tiny', 'small', 'large', 'huge', 'gigantic', 'bigdata'):
-            Radiobutton(self, variable = self.workload, text = r,
-                        value = r).grid(row = 1, column = col)
+            Radiobutton(self, variable = self.workload, text = r, value = r).grid(row = 1, column = col)
             col += 2
 
         self.task_label = tk.Label(self, text='Select Task: ')
         self.task_label.grid(row = 2, column = 0, padx = 5, pady = 5, sticky = tk.W + tk.E)
 
-        self.hibench_task = StringVar()
         col = 2
         for r in ('WordCount', 'Sort', 'TeraSort', 'linear', 'SVD', 'KMeans', 'PageRank'):
-            Radiobutton(self, variable = self.hibench_task, text = r,
-                        value = r).grid(row = 2, column = col)
+            Radiobutton(self, variable = self.hibench_task, text = r, value = r).grid(row = 2, column = col)
             col += 2
 
-        self.submit_button = tk.Button(self, text = 'Add', command = self.button_click())
+        self.submit_button = tk.Button(self, text = 'Add', command = self.add_task)
         self.submit_button.grid(row = 5, column = 5)
 
+        Label(self, text='Task Summary: ').grid()
 
-    def button_click(self):
-        print self.workload
-        print self.hibench_task
+
+    def add_task(self):
+        value = []
+        value.append(self.workload.get())
+        value.append(self.hibench_task.get())
+        global task_id, hibench_parameter
+        hibench_parameter.append(value)
+
+        Label(self, text='Task' + str(task_id) + ': ' + hibench_parameter[task_id][1]
+                         + ', Workload: '+ hibench_parameter[task_id][0])\
+            .grid(column=2, columnspan=6)
+        print "finish add task"
+        task_id += 1
 
 
 '''
@@ -253,8 +314,8 @@ class OpentunerPSFrame(BaseFrame):
         self.ot_st_label = Label(self, text='Search Technique: ')
         self.ot_st_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        st = StringVar()
-        self.search_technique = ttk.Combobox(self, textvariable=st)
+        self.st = StringVar()
+        self.search_technique = ttk.Combobox(self, textvariable=self.st)
         self.search_technique['values'] = ('AUC Bandit', 'Other')
         self.search_technique['state'] = 'readonly'
         self.search_technique.current(0)
@@ -271,8 +332,8 @@ class OpentunerPSFrame(BaseFrame):
         self.ot_tc_label = Label(self, text='Tune Condition: ')
         self.ot_tc_label.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
 
-        tc = StringVar()
-        self.tune_condition = ttk.Combobox(self, textvariable=tc)
+        self.tc = StringVar()
+        self.tune_condition = ttk.Combobox(self, textvariable=self.tc)
         self.tune_condition['values'] = ('Time', 'Other')
         self.tune_condition['state'] = 'readonly'
         self.tune_condition.current(0)
@@ -280,8 +341,17 @@ class OpentunerPSFrame(BaseFrame):
         self.tune_condition.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
 
         # Save Button
-        self.save_button = tk.Button(self, text='Save')
+        self.save_button = tk.Button(self, text='Save', command=self.save_opentuner)
         self.save_button.grid(row=3, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+
+    def save_opentuner(self):
+        value=[]
+        value.append(self.st.get())
+        value.append(self.ot_limit.get())
+        value.append(self.tc.get())
+
+        global opentuner_parameter
+        print value
 
 '''
 haven't implement 
@@ -307,8 +377,19 @@ class PSSummaryFrame(BaseFrame):
         self.menu_bar.add_command(label='Runtime Monitor',
                                   command=lambda: self.controller.show_frame(RuntimeFrame))
 
+
+
+
         self.spark_summary_label = Label(self, text='Spark Summary: ')
         self.spark_summary_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        global spark_parameter, hibench_parameter, opentuner_parameter
+
+        print len(spark_parameter), len(hibench_parameter), len(opentuner_parameter)
+
+
+        for i in spark_parameter:
+            Label(self, text=str(i)).grid()
 
         # table:
 
@@ -316,10 +397,14 @@ class PSSummaryFrame(BaseFrame):
         self.hibench_summary_label.grid(row=5, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
 
         # table:
+        for i in hibench_parameter:
+            Label(self, text=str(i)).grid()
 
         self.opentuner_summary_label = Label(self, text='Opentuner Summary: ')
         self.opentuner_summary_label.grid(row=10, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
 
+        for i in opentuner_parameter:
+            Label(self, text=str(i)).grid()
 
         self.submittasks = tk.Button(self, text='Submit Tasks')
         self.submittasks.grid(padx=5, pady=5, sticky=tk.W + tk.E)
